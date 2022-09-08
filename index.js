ac7365e09cb7b74eba2643c81ca90c5071ce144e
manivasagam <s.kishore123.64@gmail.com>
2022-09-08 09:16:27 +0530

    task done

---
+++ index.js
@@ -0,0 +1,48 @@
+let container = document.createElement("div");
+container.setAttribute("class","conatiner");
+let span = document.getElementById("span");
+span.innerHTML = 10;
+document.body.append(container);
+let n = 10;
+
+setTimeout(()=>{
+    n = n-1
+    span.innerHTML = n
+setTimeout(()=>{
+    n = n-1
+    span.innerHTML = n
+setTimeout(()=>{
+    n = n-1
+    span.innerHTML = n
+setTimeout(()=>{
+    n = n-1
+    span.innerHTML = n
+setTimeout(()=>{
+    n = n-1
+    span.innerHTML = n
+ setTimeout(()=>{
+    n = n-1
+    span.innerHTML = n
+setTimeout(()=>{
+    n = n-1
+    span.innerHTML = n
+setTimeout(()=>{
+    n = n-1
+    span.innerHTML = n
+setTimeout(()=>{
+    n = n-1
+    span.innerHTML = n
+setTimeout(()=>{
+    n = n-1
+    span.innerHTML = "Happy Independence Day🎉"
+                                      
+    },1000)
+    },1000)
+    },1000)
+    },1000)
+    },1000)
+    },1000)
+    },1000)
+    },1000)
+    },1000)
+    },1000)
